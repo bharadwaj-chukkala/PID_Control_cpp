f3ce5f029a30162bd144bb8fc769660dd44e7892
bharadwaj-chukkala <bchukkal@terpmail.umd.edu>
2023-07-10 01:17:18 -0400

    Adding header files

---
+++ include/pid.h
@@ -0,0 +1,39 @@
+#ifndef PID_CONTROLLER_H
+#define PID_CONTROLLER_H
+
+typedef struct {
+    /* Controller Gains */
+    float Kp;
+    float Ki;
+    float Kd;
+
+    /* Derivative Low pass filter */
+    float tau;
+
+    /* Output Limits */
+    float limMin;
+    float limMax;
+
+    /* Integrator Limits */
+    float limMinInt;
+    float limMaxInt;
+
+    /* Sample Time (in seconds) */
+    float T;
+
+    /* Controller "memory" */
+    float integrator;
+    float prevError;            // Required for integrator
+    float differentiator;
+    float prevMeasurement;      // Required for differentiator
+
+    /* Controller Output */
+    float out;
+
+} PIDController;
+
+void PIDController_Init(PIDController *pid);
+float PIDController_Update(PIDController *pid, float setpoint, float measurement);
+
+
+#endif
